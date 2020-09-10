95004c154f5e88f111a8a26238fa4ac7b187f562
Charles <de Gheldere>
2020-09-10 20:51:51 +0200

    new project requests && reviews && ajax (unstyled)

--- app/models/review.rb
+++ app/models/review.rb
@@ -1,6 +1,6 @@
 class Review < ApplicationRecord
   belongs_to :user
-  belongs_to :project
+  belongs_to :project, optional: true
   validates :content, presence: true
   validates :rating,  length: { in: 1..5 }
 end
---
+++ test/controllers/new_project_requests_controller_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class NewProjectRequestsControllerTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+end
