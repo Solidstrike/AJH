520a3988bb33086ccb07b5d97bf7e7a2e2777ae9
Roman <frontandstart@gmail.com>
2020-09-11 03:59:38 +0300

    f

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
-  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   root to: 'pages#home'
   match 'users' => 'users#show', via: :get
 
